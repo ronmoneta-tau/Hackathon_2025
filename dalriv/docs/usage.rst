0ab1a01eb1bae1920b765c1e9d3bf4ec6177ce6d
almogalf <almogalfamon629@gmail.com>
2025-07-02 11:47:10 +0300

    Cleaned main branch

--- dalriv/docs/usage.rst
+++
@@ -1,7 +0,0 @@
-=====
-Usage
-=====
-
-To use Dalriv in a project::
-
-    import dalriv
--- dalriv/src/dalriv/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Top-level package for Dalriv."""
-
-__author__ = """Reut"""
-__email__ = 'ronmoneta@mail.tau.ac.il'
-__version__ = '0.1.0'
