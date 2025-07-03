0d34448637f7074723dccb48489b7ee801127aab
Shira Zadok <shirazadok@mail.tau.ac.il>
2025-07-03 11:54:11 +0300

    Updating test code according to PEP8

--- src/test_DataLoader.py
+++ src/test_DataLoader.py
@@ -1,4 +1,3 @@
-
 folder_path = "/Users/user/Downloads/hackathon_2025"
 from data_loader import DataLoader
 import pytest
@@ -8,23 +7,28 @@ from pathlib import Path
 import os
 from unittest.mock import patch, mock_open
 
+
 # Test if the folder path is acceptable:
 def test_invalid_folder_path():
     invalid_path = "/invalid/nonexistent/folder/path"
     with pytest.raises(FileNotFoundError):
         DataLoader(invalid_path)
 
+
 def test_file_instead_folder():
-    invalid_path = "/Users/user/Documents/2nd_Degree/Courses/Python/parse_subject_names.ipynb"
+    invalid_path = (
+        "/Users/user/Documents/2nd_Degree/Courses/Python/parse_subject_names.ipynb"
+    )
     with pytest.raises(FileNotFoundError):
         DataLoader(invalid_path)
+
 
 def test_empty_input_folder_returns_empty():
     empty_folder_path = "/Users/user/Documents/2nd_Degree/Courses/Python/empty_folder"
     data_loader = DataLoader(empty_folder_path)
-    
+
     df_dict, feature_map = data_loader.get_dataframe()
-    
+
     assert df_dict == {}, "Expected empty dictionary for df_dict"
     assert feature_map == {}, "Expected empty dictionary for feature_map"
 
@@ -44,21 +48,25 @@ def test_get_dataframe():
 def test_output_length():
     # Assuming you have a mock or real folder path
     data_loader = DataLoader(folder_path)  # Replace with the correct path
-    
+
     # Call the get_dataframe method
     df_dict, feature_map = data_loader.get_dataframe()
-    
+
     # Test if the length of the first dictionary (df_dict) is 5
-    assert len(df_dict) == 5, f"Expected df_dict to have length 5, but got {len(df_dict)}"
+    assert (
+        len(df_dict) == 5
+    ), f"Expected df_dict to have length 5, but got {len(df_dict)}"
 
 
 def test_handle_demographic():
-    clinical_file_path = os.path.join("/Users/user/Downloads/hackathon_2025/clinical", "demographic_and_clinical.xlsx")
+    clinical_file_path = os.path.join(
+        "/Users/user/Downloads/hackathon_2025/clinical", "demographic_and_clinical.xlsx"
+    )
     data_loader = DataLoader(folder_path)
     result_name, result_df = data_loader.handle_demographic(clinical_file_path)
-    
+
     assert result_name == "clinical"
-    
+
     assert not result_df.empty
     assert "ID" in result_df.columns
 
@@ -67,29 +75,33 @@ def test_handle_cog():
     # Simulate the contents of the 'first_task_made' DataFrame
     first_task_data = {
         "ID": ["rn23001", "rn23004", "rn23010"],  # Replace with appropriate IDs
-        "CONDITION": ["stop_it", "tap_it", "tap_it"]  # Example conditions
+        "CONDITION": ["stop_it", "tap_it", "tap_it"],  # Example conditions
     }
     first_task_df = pd.DataFrame(first_task_data)
 
     data_loader = DataLoader(folder_path)
     data_loader.first_task_made = first_task_df
 
-    cog_file_path = os.path.join("/Users/user/Downloads/hackathon_2025/tasks", "stop_it_with_code_book.xlsx")
+    cog_file_path = os.path.join(
+        "/Users/user/Downloads/hackathon_2025/tasks", "stop_it_with_code_book.xlsx"
+    )
     result_name, result_df = data_loader.handle_cog(cog_file_path)
-    
+
     assert result_name == "stop_it"
-    
+
     assert not result_df.empty
     assert "ID" in result_df.columns
 
 
 def test_handle_physio():
-    clinical_file_path = os.path.join("/Users/user/Downloads/hackathon_2025/physio", "statistics_hr.xlsx")
+    clinical_file_path = os.path.join(
+        "/Users/user/Downloads/hackathon_2025/physio", "statistics_hr.xlsx"
+    )
     data_loader = DataLoader(folder_path)
     result_name, result_df = data_loader.handle_physio(clinical_file_path)
-    
+
     assert result_name == "HR"
-    
+
     assert not result_df.empty
     assert "ID" in result_df.columns
 
@@ -97,8 +109,11 @@ def test_handle_physio():
 def test_filter_participants_in_physio():
     data_loader = DataLoader(folder_path)
     df = pd.DataFrame({"ID": [1, 2, 2, 3, 4], "value": [10, 20, 20, 30, 40]})
-    filtered_df = data_loader.filter_participants_in_physio(df, 2, "removed_physio_test.txt")
+    filtered_df = data_loader.filter_participants_in_physio(
+        df, 2, "removed_physio_test.txt"
+    )
     assert filtered_df.shape[0] == 2
+
 
 def test_gets_measurement_name_without_prefix():
     file_name = "/path/to/HR.xlsx"
@@ -106,6 +121,5 @@ def test_gets_measurement_name_without_prefix():
     assert result == "HR"
 
 
-
-if __name__ == "__main__": # pragma: no cover
+if __name__ == "__main__":  # pragma: no cover
     pytest.main()
